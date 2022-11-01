b5635acd6a47414a7268bb1d0468ccc71423fbfd
IsiteYves <yvesisite@gmail.com>
2022-11-02 00:18:07 +0200

    feat: crawler setup

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const browserObject = require("./browser");
-const scraperController = require("./pageController");
+const express = require("express");
+const app = express();
+const Crawler = require("Crawler");
+require("dotenv").config();
 
-//Start the browser and create a browser instance
-let browserInstance = browserObject.startBrowser();
+const port = process.env.PORT || 8000;
 
-// Pass the browser instance to the scraper controller
-scraperController(browserInstance);
+app.get("/", (req, res) => {
+  var c = new Crawler({
+    address: "http://www.example.com",
+    term: "example",
+  });
+  c.start();
+});
+
+app.get("/scrape", (req, res) => {
+  const browserObject = require("./browser");
+  const scraperController = require("./pageController");
+
+  //Start the browser and create a browser instance
+  let browserInstance = browserObject.startBrowser();
+
+  // Pass the browser instance to the scraper controller
+  scraperController(browserInstance);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
