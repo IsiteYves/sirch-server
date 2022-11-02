4560b82931cb6af3bce985f759315ac388ecceed
IsiteYves <yvesisite@gmail.com>
2022-11-02 21:42:22 +0200

    feat: add try...catch

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 
 const port = process.env.PORT || 8000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/", puppeteerLookup);
 
 app.get("/scrape", (req, res) => {
